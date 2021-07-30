45090453f710323c8450b74ed56c29b13c9c9dc1
jexists <jexists486@gmail.com>
2021-07-30 22:26:34 +0900

    deadlock

---
+++ m_go/tucker/accout_deadlock/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type Account struct {
+	balance int
+	mutex   *sync.Mutex
+}
+
+func (a *Account) Widthdraw(val int) {
+	a.balance -= val
+}
+
+func (a *Account) Deposit(val int) {
+	a.balance += val
+}
+
+func (a *Account) Balance() int {
+	balance := a.balance
+	return balance
+}
+
+var accounts []*Account
+
+func Transfer(sender, receiver int, money int) {
+	accounts[sender].Widthdraw(money)
+	accounts[receiver].Deposit(money)
+
+	fmt.Println("Transfer", sender, receiver, money)
+}
+
+func GetTotalBalance() int {
+	total := 0
+	for i := 0; i < len(accounts); i++ {
+		total += accounts[i].Balance()
+	}
+	return total
+}
+
+func RandomTranfer() {
+	var sender, balance int
+	for {
+		sender = rand.Intn(len(accounts))
+		balance = accounts[sender].Balance()
+		if balance > 0 {
+			break
+		}
+	}
+
+	var receiver int
+	for {
+		receiver = rand.Intn(len(accounts))
+		if sender != receiver {
+			break
+		}
+	}
+
+	money := rand.Intn(balance)
+	Transfer(sender, receiver, money)
+}
+
+func GoTransfer() {
+	for {
+		RandomTranfer()
+	}
+}
+
+func PrintTotalBalance() {
+	fmt.Printf("Total: %d\n", GetTotalBalance())
+}
+
+func main() {
+	for i := 0; i < 20; i++ {
+		accounts = append(accounts, &Account{balance: 1000, mutex: &sync.Mutex{}})
+	}
+
+	go func() {
+		for {
+			Transfer(0, 1, 100)
+		}
+	}()
+
+	go func() {
+		for {
+			Transfer(1, 0, 100)
+		}
+	}()
+
+	for { //thread
+		// PrintTotalBalance()
+		time.Sleep(100 * time.Millisecond)
+	}
+}
