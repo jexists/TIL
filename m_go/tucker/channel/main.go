2c41fda0c5fe7900c7d1bf857f03b4264ad91dfd
jexists <jexists486@gmail.com>
2021-08-01 02:25:31 +0900

    channel

--- m_go/tucker/channel/main.go
+++ m_go/tucker/channel/main.go
@@ -1,12 +1,20 @@
 package main
 
-func main() {
-	var c chan int        //선언
-	c = make(chan int, 1) //초기화
-	// c = make(chan int) // <- DeadLock발생
+import "fmt"
 
-	c <- 10
+func pop(c chan int) {
+	println("Pop Func")
 	v := <-c
+	fmt.Println(v)
+}
+func main() {
+	var c chan int //선언
+	// c = make(chan int, 1) //초기화
+	c = make(chan int) // <- DeadLock발생
+	go pop(c)
+	c <- 10
+	// v := <-c
 
-	println(v) //10
+	// println(v) //10
+	println("End of Program")
 }
