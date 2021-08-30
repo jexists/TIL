8034fb68a61ee61491b291516f032a3641277eb7
jexists <jexists486@gmail.com>
2021-08-30 21:53:03 +0900

    메소드 체이닝

--- l_rxjs/func-programming/fpt.js
+++ l_rxjs/func-programming/fpt.js
@@ -5,11 +5,11 @@ const array = [0, 1, 2, 3, 4, 5];
 //   const item = array[i];
 //   array[i] = item + 1;
 // }
-console.log(array);
+console.log(array); // [ 0, 1, 2, 3, 4, 5 ]
 
 // 선언적 프로그래밍 -> 순수 함수
 const newArray = array.map(v => v + 1);
-console.log(newArray);
+console.log(newArray); // [ 1, 2, 3, 4, 5, 6 ]
 
 let counter = 0;
 function add(a, b) {
@@ -29,20 +29,54 @@ console.log(add(1, 2)); // 3
 // 고차함수
 
 function greaterThen(n) {
-  return function(m) {
+  return function (m) {
     return m > n;
   }
 }
 
 const greaterThen10 = greaterThen(10);
 
-const result = greaterThen(11);
-console.log(result);
-const result = greaterThen(9);
-console.log(result);
+const result1 = greaterThen(11);
+console.log(result1); // [Function]
+const result2 = greaterThen(9);
+console.log(result2); // [Function]
 
 function forEach(array, action) {
   for (var i = 0; i < array.length; i++) action(array[i]);
 }
 
 forEach(["hello", "higher", "order"], console.log);
+// hello
+// higher
+// order
+const array2 = [0, 1, 2, 3, 4, 5];
+
+// const newArray2 = array2.map(v => {
+//   return v * 2;
+// })
+const newArray2 = array2.map(v => v * 2);
+const filterArray = array2.filter(v => v % 2 === 0);
+const sum = array.reduce((prev, curr) => {
+  return prev + curr;
+}, 0);
+console.log(array2); // [ 0, 1, 2, 3, 4, 5 ]
+console.log(newArray2); // [ 0, 2, 4, 6, 8, 10 ]
+console.log(filterArray); // [ 0, 2, 4 ]
+console.log(sum); // 15
+
+
+// 매소드 체이닝 
+// array.map(v => v * 2)
+//   .filter(v => v % 2 == 0)
+//   .reduce((prev, curr) => {
+//     return prev + curr;
+//   }, 0);
+
+const double = v => v * 2;
+const isEven = v => v % 2 == 0;
+const sumUp = (prev, curr) => prev + curr;
+const final = array2.map(double)
+  .filter(isEven)
+  .reduce(sumUp);
+console.log(array2); // [ 0, 1, 2, 3, 4, 5 ]
+console.log(final); // 30
