81c3dfd85951f6e197b8cae5f21206a3cc0db039
jexists <jexists486@gmail.com>
2021-09-11 22:43:36 +0900

    operator

--- l_rxjs/01-start/rxjs-operator.js
+++ l_rxjs/01-start/rxjs-operator.js
@@ -1,4 +1,5 @@
 import { Observable, from } from "rxjs";
+import { map, filter, tap, scan, reduce } from "rxjs/operators";
 
 // [1, 2, 3].map(v => v + 1)
 
@@ -27,13 +28,32 @@ const myMap = (mapFn) => (observable) =>
   });
 
 const myObservable = from([1, 2, 3, 4])
-const newObservable = myMap(v => v + 1)(myObservable)
-newObservable.subscribe(console.log)
+// const newObservable = myMap(v => v + 1)(myObservable)
+// newObservable.subscribe(console.log)
 // chaining
-myMap(v => v + 1)(newObservable).subscribe(console.log)
+// myMap(v => v + 1)(newObservable).subscribe(console.log)
 
-myMap(v => v + 1)(
-  myMap(v => v + 1)(
-    myMap(v => v + 1)(myObservable)
-  )
-).subscribe(console.log)
+// myMap(v => v + 1)(
+//   myMap(v => v + 1)(
+//     myMap(v => v + 1)(myObservable)
+//   )
+// ).subscribe(console.log)
+
+// -> 안깔끔해서 rxjs에서는 pipe를 제공
+
+// from([1, 2, 3, 4])
+//   .pipe(
+//     myMap(v => v + 1),
+//     myMap(v => v + 1),
+//     myMap(v => v + 1)
+//   ).subscribe(console.log)
+
+from([1, 2, 3, 4])
+  .pipe(
+    map(v => v + 1),
+    tap(v => console.log("TAP", v)),
+    filter(v => v % 2 === 0),
+    tap(v => console.log("filter", v)),
+    // scan((x, y) => x + y, 0),
+    reduce((x, y) => x + y, 0)
+  ).subscribe(console.log)
