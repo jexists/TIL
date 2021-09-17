54b92cf7af7aeac12346af87685d89af3f88efdb
jexists <jexists486@gmail.com>
2021-09-17 23:47:41 +0900

    reduce example

--- b_vanillaJS/fastcamp/reduce.js
+++ b_vanillaJS/fastcamp/reduce.js
@@ -20,3 +20,16 @@ const sum = numbers.reduce((accumulator, current, index, array) => {
 console.log(sum);
 
 // reduce((accumulator누적된값, current현재값, index몇번째아이템, array총자신)=> 어떻게 연산할지, accumulator기본값/초기값)
+
+const alphabets = ['a', 'a', 'a', 'b', 'c', 'c', 'd', 'e'];
+
+const counts = alphabets.reduce((acc, cur) => {
+  if (acc[cur]) {
+    acc[cur] += 1;
+  } else {
+    acc[cur] = 1;
+  }
+  return acc
+}, {})
+
+console.log(counts);
