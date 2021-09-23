98168b4534d08c451a7d12779c8e9596c693cbcb
jexists <jexists486@gmail.com>
2021-09-23 22:30:27 +0900

    catchError

--- l_rxjs/fastcampus/rxjs-exception.js
+++ l_rxjs/fastcampus/rxjs-exception.js
@@ -1,5 +1,5 @@
-import { interval, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { interval, Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 // new Observable(observer => {
 //   observer.next(1);
@@ -10,12 +10,41 @@ import { map } from 'rxjs/operators';
 //   observer.next(4);
 // }).subscribe(console.log)
 
-new Observable(observer => {
-  observer.next(1);
-  observer.next(2);
-  observer.error(new Error("ERROR")); // throw new Error("ERROR"); 동일한 에러
-  // throw new Error("ERROR");
-  observer.next(3);
-  observer.next(4);
-}).subscribe(console.log, console.error)
+// new Observable(observer => {
+//   observer.next(1);
+//   observer.next(2);
+//   observer.error(new Error("ERROR")); // throw new Error("ERROR"); 동일한 에러
+//   // throw new Error("ERROR");
+//   observer.next(3);
+//   observer.next(4);
+// }).subscribe(console.log, console.error)
 
+// interval(100)
+//   .pipe(map(x => {
+//     if(x===1) {
+//       throw new Error('ERROR');
+//     }
+//     return x;
+//   })).subscribe(
+//     console.log, 
+//     e => {
+//       console.error('error', e);
+//   },
+//     _ => console.log('complete') //호출 안됨
+//   )
+
+interval(100)
+  .pipe(map(x => {
+    if (x === 1) {
+      throw new Error('ERROR');
+    }
+    return x;
+  }),
+    catchError(e => of(1))
+    ).subscribe(
+      console.log,
+      e => {
+        console.error('error', e);
+      },
+      _ => console.log('complete') //호출 안됨
+    )
