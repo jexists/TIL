83ea5838f8a8d3eed952d813a210c6858011d576
jexists <jexists486@gmail.com>
2021-10-17 21:34:20 +0900

    cluster

--- d_angular/naver-map/src/app/map-cluster/marker-clustering.service.ts
+++ d_angular/naver-map/src/app/map-cluster/marker-clustering.service.ts
@@ -4,27 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MarkerClusteringService {
-  _clusterCenter
-  _clusterBounds
-  _clusterMarker
-  _relation
-  _clusterMember
-  _markerClusterer
 
   constructor(
-    markerClusterer
+    
   ) {
-
-    this._clusterCenter = null;
-    this._clusterBounds = null;
-    this._clusterMarker = null;
-    this._relation = null;
-
-    this._clusterMember = [];
-
-    this._markerClusterer = markerClusterer;
   }
 
-  
+  MarkerClustering = function () {
+    
+  }
+
 
 }
