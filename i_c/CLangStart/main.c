f2e28be50116b323ebc21e0dfd4344a531e3ebd6
jexists <jexists486@gmail.com>
2021-11-12 21:31:21 +0900

    folder 정리

---
+++ i_c/CLangStart/main.c
@@ -0,0 +1,55 @@
+//
+//  main.c
+//  CLangStart
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    //  Created by jexists on 2020/11/28.
+//    //변수 데이터 타입 공부
+//    int age = 27;
+//    float weight;
+//    char letter_grade = 'a';
+//
+//
+//    printf("%d \n", age);
+//
+//    printf("I am %d years old \n", age);
+//
+//    printf("\t I am %fkg \n", weight);
+//    weight = 40.5;
+//
+//    printf("\t I am %fkg \n", weight);
+//    printf("\t I am %.2fkg \n", weight);
+//
+//    printf("I got %c \n", letter_grade);
+//
+//    printf("Hello, World! Ju Ae\n");
+    
+    
+    //  Created by jexists on 2020/12/05.
+    //연산자
+    
+//    int a = 5 + 10;
+    
+//    int a = 10 % 5;
+//
+//    printf("%d \n", a);
+    
+    
+      int a = 1;
+      int b = 5;
+
+      printf("a==b : %d \n", a==b);
+      printf("a!=b : %d \n", a!=b);
+      printf("a<b : %d \n", a<b);
+      printf("a>b : %d \n", a>b);
+      printf("a<=b : %d \n", a<=b);
+      printf("a>=b : %d \n", a>=b);
+    
+    return 0;
+}
