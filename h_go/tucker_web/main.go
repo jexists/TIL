77ae989ef8dbd2a7655f1c9be9dd75f9d8c59464
jexists <jexists486@gmail.com>
2021-11-16 22:52:30 +0900

    handler

--- h_go/tucker_web/main.go
+++ h_go/tucker_web/main.go
@@ -5,10 +5,24 @@ import (
 	"net/http"
 )
 
+type fooHandler struct{}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Foo")
+}
+
 func main() {
+	// http.HandleFunc("경로", func(w 응답받는 writer, r *사용자가 요청한 request) { 경로일 경우 실행되는 함수})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello world")
+		fmt.Fprint(w, "Hello world") //writer에다가 print
 	})
 
-	http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello Bar")
+	})
+
+	// instance로  (interface)
+	http.Handle("/foo", &fooHandler{})
+
+	http.ListenAndServe(":3000", nil) // 웹서버 실행
 }
