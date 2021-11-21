982886cd2eb1d7371480cf1046cd06c13fce5d52
jexists <jexists486@gmail.com>
2021-11-21 09:38:20 +0900

    test 환경

--- h_go/tucker_web/main.go
+++ h_go/tucker_web/main.go
@@ -1,65 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
+	"tucker_web/myapp"
 )
-
-type User struct {
-	// FirstName string
-	// LastName  string
-	// Email     string
-	// CreateAt  time.Time
-
-	//annotaion
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
-	CreateAt  time.Time `json:"create_at"`
-}
-
-type fooHandler struct{}
-
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	user := new(User)
-	err := json.NewDecoder(r.Body).Decode(user)
-	if err != nil {
-		w.Header().Add("content-type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err)
-		return
-	}
-
-	user.CreateAt = time.Now()
-	//json 형태로 변경
-	data, _ := json.Marshal(user)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, string(data))
-
-}
-
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "world~"
-	}
-	fmt.Fprintf(w, "Hello %s", name)
-}
 
 func main() {
 
-	// instance 만들어서 등록
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello world") //writer에다가 print
-	})
-
-	mux.HandleFunc("/bar", barHandler)
-
-	mux.Handle("/foo", &fooHandler{})
-
-	http.ListenAndServe(":3000", mux) // 웹서버 실행
+	http.ListenAndServe(":3000", myapp.NewHttpHandler())
 }
