922300e5067dc5041dd8743e5e1b490bc958f577
jexists <jexists486@gmail.com>
2021-12-07 23:06:14 +0900

    반환값 함수

--- b_typeScript/essential/first-class-function.ts
+++ b_typeScript/essential/first-class-function.ts
@@ -25,3 +25,29 @@ const htmlOL = makeLi(ol, ['봄', '여름', '가을', '겨울']);
 
 console.log(htmlUL);
 console.log(htmlOL);
+
+
+// 반환값으로 함수인 경우 =======
+
+function salePrice(discountRate, price) {
+  return price - (price * (discountRate * 0.01));
+}
+
+console.log('여름 세일 - ', salePrice(30, 567000));
+console.log('겨울 세일 - ', salePrice(10, 567000));
+
+function discountPrice(discountRate) {
+  return function (price) {
+    return price - (price * (price * 0.01))
+  }
+}
+
+console.log('여름 세일 - ', discountPrice(30)(567000));
+console.log('겨울 세일 - ', discountPrice(10)(567000));
+
+let summerPrice = discountPrice(30);
+let winterPrice = discountPrice(10);
+
+// 변수에 넣을 수 있다는 장점이 생겨서 좋음 (아래 코드가 알기 쉽다.) -> 표현력 극대화
+console.log('여름 세일 - ', summerPrice(567000));
+console.log('겨울 세일 - ', winterPrice(567000));
