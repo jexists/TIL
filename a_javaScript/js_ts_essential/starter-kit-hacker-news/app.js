0686904af3a24fec15a05bd425ecabac22f2fbe4
jexists <jexists486@gmail.com>
2022-01-21 23:18:47 +0900

    code  정리

--- a_javaScript/js_ts_essential/starter-kit-hacker-news/app.js
+++ a_javaScript/js_ts_essential/starter-kit-hacker-news/app.js
@@ -1,21 +1,28 @@
 
 // https://github.com/tastejs/hacker-news-pwas/blob/master/docs/api.md
 
-const ajax = new XMLHttpRequest();
-
+const container = document.getElementById('root');
 const content = document.createElement('div');
+
+const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-ajax.open('GET', NEWS_URL, false);
 
-ajax.send();
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
 
-console.log(ajax.respense);
+  return JSON.parse(ajax.response);
+}
 
-// 응답값을 객체로
-const newsFeed = JSON.parse(ajax.response);
-console.log(newsFeed);
+const newsFeed = getData(NEWS_URL);
 
+// ajax.open('GET', NEWS_URL, false);
+// ajax.send();
+// console.log(ajax.respense);
+// // 응답값을 객체로
+// const newsFeed = JSON.parse(ajax.response);
+// console.log(newsFeed);
 
 // document.getElementById('root').innerHTML = `
 //   <ul>
@@ -30,35 +37,39 @@ const ul = document.createElement('ul');
 window.addEventListener('hashchange', function () {
   // console.log('해쉬변경');
   // console.log(location.hash);
+  
   const id = location.hash.substring(1);
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  // ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  // ajax.send();
+  // const newsContent = JSON.parse(ajax.response);
+  
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
   const title = document.createElement('h1');
   title.innerHTML = newsContent.title;
 
   content.appendChild(title);
   console.log(newsContent);
-
-
 });
 
 for (let i = 0; i < 10; i++) {
-  const li = document.createElement('li');
-  const a = document.createElement('a');
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <li>
+  <a href="#${newsFeed[i].id}">${newsFeed[i].title} [${newsFeed[i].comments_count}]</a>
+  </li>
+  `;
 
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} [${newsFeed[i].comments_count}]`
+  ul.appendChild(div.children[0]); // == ul.appendChild(div.firstElementChild);
 
-  a.addEventListener('click', function () {
-
-  })
-
+  // const li = document.createElement('li');
+  // const a = document.createElement('a');
+  // a.href = `#${newsFeed[i].id}`;
+  // a.innerHTML = `${newsFeed[i].title} [${newsFeed[i].comments_count}]`
+  // a.addEventListener('click', function () {})
   // li.innerHTML = newsFeed[i].title;
-  li.appendChild(a);
-  ul.appendChild(li);
+  // li.appendChild(a);
+  // ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul)
-document.getElementById('root').appendChild(content)
+container.appendChild(ul)
+container.appendChild(content)
