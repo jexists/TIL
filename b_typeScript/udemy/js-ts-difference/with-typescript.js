23a0f4364f6b30871ea7f5e339d6d6b5835187d2
jexists <jexists486@gmail.com>
2022-01-29 22:11:54 +0900

    타입스크립트 타입

---
+++ b_typeScript/udemy/js-ts-difference/with-typescript.js
@@ -0,0 +1,6 @@
+function addTS(a, b) {
+    return a + b;
+}
+var resultTS = addTS(2, 5); // 7
+// const resultTS = addTS('2', '5'); // Type ERROR
+console.log(resultTS);
---
+++ b_typeScript/udemy/js-ts-difference/with-typescript.ts
@@ -0,0 +1,7 @@
+function addTS(a:number, b: number): number {
+  return a + b;
+}
+
+const resultTS = addTS(2, 5); // 7
+// const resultTS = addTS('2', '5'); // Type ERROR
+console.log(resultTS);
