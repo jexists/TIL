dc6a9a3a9c7ce3d3fe5cc9c9e185cbfdaa38a99c
jexists <jexists486@gmail.com>
2022-03-10 22:46:08 +0900

    currying map

--- b_typeScript/functional_programming/currying.ts
+++ b_typeScript/functional_programming/currying.ts
@@ -1,5 +1,7 @@
 
 
+import * as O from "./cart_option";
+
 // Currying 커링
 //  -> 인자가 여러개인 함수를 인자가 하나인 함수들의 함수열
 
@@ -39,5 +41,54 @@ export const main = () => {
 
   const momsPresent2 = curriedDelivery("상품권")("엄마");
   console.log(momsPresent2("딸"));
-  
+
+}
+
+
+export const curry2 = <A, B, C>(f: (a: A, b: B) => C) => (a: A) => (b: B): C => f(a, b);
+
+export const flip = <A, B, C>(f: (a: A, b: B) => C) => (b: B, a: A): C => f(a, b);
+
+// Array<A> == A[]
+// map :: (Array<A>, (A => B)) => Array<B>
+
+export const map = <A, B>(array: Array<A>, f: (a: A) => B): Array<B> => {
+  const result: Array<B> = [];
+  for (const value of array) {
+    result.push(f(value));
+  }
+  return result;
+};
+
+export const main3 = () => {
+  const numbers = [1, 2, 3];
+  const isEven = (x: number) => x % 2 === 0;
+
+  map(numbers, isEven);
+
+  // curriedMap :: Array<A> => ((A => B) => Array<B>)
+  const curriedMap = curry2(map);
+  curriedMap(numbers)(isEven);
+
+  // Array<A>.map :: (A => B) => Array<B>
+  // map :: Array<A> ~> (A => B) => Array<B>
+  numbers.map(isEven);
+
+  // map_ :: (A => B) => (Array<A> => Array<B>)
+  const map_ = curry2(flip(map));
+  // map_(isEven)(numbers);
+  // isEven :: number => boolean
+  // mapIsEven :: Array<number> => Array<boolean>
+  const mapIsEven = map_(isEven);
+
+  isEven(42);
+  isEven(7);
+  mapIsEven([42]);
+
+  const omap = curry2(flip(O.map));
+  //optionIsEven :: Option<number> => Option<boolean>
+  const optionIsEven = omap(isEven);
+
+  optionIsEven(O.some(42));
+  optionIsEven(O.none());
 }
